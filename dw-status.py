af4f554b31e6b28889a8a0eb9573d5705b12c1d2
Paulo Henrique Silva <ph.silva@gmail.com>
2017-02-18 15:41:37 -0200

    colors on dw-status.py

--- dw-status.py
+++ dw-status.py
@@ -1,5 +1,10 @@
 import sys
 
+
+def red(msg, end=None):
+    print("\033[1;31m%s\033[0m" % msg, end=end)
+
+
 reg = int(sys.argv[1], 16)
 
 if len(sys.argv) >= 3:
@@ -8,7 +13,11 @@ else:
     size = 32
 
 for i in range(size - 1, -1, -1):
-    print("%02d" % i, end='|')
+    on = reg & (1 << i)
+    if on:
+        red("%02d" % i, end='|')
+    else:
+        print("%02d" % i, end='|')
 print()
 
 for i in range(size - 1, -1, -1):
